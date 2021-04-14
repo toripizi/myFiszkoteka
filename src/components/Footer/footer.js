d5f2b68162ad054dab17b587d247e68fc3547cf6
DESKTOP-64LQP7A\Maciej <maciekgoncerzewicz@gmail.com>
2021-04-14 21:09:33 +0200

    More mobile style

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,5 +1,11 @@
 export default function Footer() {
    return (
-      <footer></footer>
+      <footer className="flex flex-ai-c flex-jc-c flex-column">
+         <span>
+            Fiszkoteka, <br></br>
+            Projekt graficzny strony zaczerpnięty z challengu Fronted Mentor.
+            <i className="icon-mail-1" style={{ marginLeft: "10px", padding: "0" }}>maciekgoncerzewicz@gmail.com</i>
+         </span>
+      </footer>
    );
 }
--- src/components/Header/menu.js
+++ src/components/Header/menu.js
@@ -3,18 +3,32 @@ import ChangeTheme from './changeTheme/changeTheme';
 
 function Menu() {
    const zakryj = () => {
-      document.querySelector('#mobileMenu').style.transform = 'translate(50vw, 0)'
+      document.querySelector('#mobileMenu').style.transform = 'translate(100vw, 0)'
    }
 
    return (
       <div id="mobileMenu" className="hide-for-desktop flex">
-         <span className="flex flex-ai-c flex-jc-sb">
+         <div id="mobileMenu-header" className="flex flex-ai-c flex-jc-sb">
             <i className="icon-cancel hide-for-desktop" onClick={zakryj}></i>
             <span>
                <ChangeTheme cn="" />
             </span>
-         </span>
-         <span>
+         </div>
+         <div className="flex flex-ai-c flex-jc-sb mobileMenu-opcja">
+            <h3>
+               <i className="icon-key"></i>
+               Zmień hasło
+            </h3>
+            <i className="icon-angle-right"></i>
+         </div>
+         <div className="flex flex-ai-c flex-jc-sb mobileMenu-opcja">
+            <h3>
+               <i className="icon-doc-add"></i>
+               Dodaj nową kolekcje
+            </h3>
+            <i className="icon-angle-right"></i>
+         </div>
+         <span id="mobileMenu-logOut">
             <Wylogowanie />
          </span>
       </div>
